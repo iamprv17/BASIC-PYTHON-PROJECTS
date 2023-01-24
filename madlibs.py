bd288fc31faeab2c25585012023053112460ea07
Pranav Asokan <91105918+iamprv17@users.noreply.github.com>
2023-01-24 22:06:17 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,11 @@
+def madlibs():
+    noun1 = input("Enter a noun: ")
+    verb1 = input("Enter a verb: ")
+    adjective1 = input("Enter an adjective: ")
+    adverb1 = input("Enter an adverb: ")
+    noun2 = input("Enter another noun: ")
+
+    print("The " + adjective1 + " " + noun1 + " " + verb1 + " " + adverb1 + " to the " + noun2 + ".")
+
+madlibs()
 
