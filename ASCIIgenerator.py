a803e5bd79fba7b475b0dc884e98dd413d8d6fe3
Pranav Asokan <91105918+iamprv17@users.noreply.github.com>
2023-01-24 22:57:31 +0530

    Create ASCIIgenerator.py

---
+++ ASCIIgenerator.py
@@ -0,0 +1,16 @@
+#ASCII Art Generator
+
+def pyramid():
+    pyramid = ["   #   ", "  ###  ", " ##### ", "#######"]
+    for row in pyramid:
+        print(row)
+
+print("Welcome to the ASCII Art Generator!")
+print("1. Pyramid")
+print("2. Exit")
+
+user_choice = int(input("Please select an option: "))
+if user_choice == 1:
+    pyramid()
+else:
+    exit()
